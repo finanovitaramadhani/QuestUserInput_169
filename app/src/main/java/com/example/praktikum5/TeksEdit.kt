1a5d6453426a63b43c8730da6883a87581c76003
finanovita <finanovita479@gmail.com>
2025-10-31 09:16:23 +0700

    membuat variabel untuk mengingat nilai masukan/yang ingin ditulis

--- app/src/main/java/com/example/praktikum5/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum5/TeksEdit.kt
@@ -4,5 +4,10 @@ package com.example.praktikum5
 
 fun FormDataDiri(modifier: Modifier
 ){
+    //variabel variabel untuk mengingat nilai masukan dari keyboard
+    var textNama by remember {mutableStateOf("")}
+    var textAlamat by remember {mutableStateOf("")}
+    var textJK by remember {mutableStateOf("")}
+
 
 }
