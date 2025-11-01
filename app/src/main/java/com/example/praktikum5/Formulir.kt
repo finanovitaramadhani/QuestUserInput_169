8deae78f4a05542dd1db940d9acdfd0b7ee8ff00
finanovita <finanovita479@gmail.com>
2025-11-01 22:40:15 +0700

    Menambahkan struktur dasar composable FormFormulir

--- app/src/main/java/com/example/praktikum5/Formulir.kt
+++ app/src/main/java/com/example/praktikum5/Formulir.kt
@@ -1,3 +1,19 @@
 package com.example.praktikum5
 
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun FormFormulir(modifier: Modifier = Modifier) {
+    }
